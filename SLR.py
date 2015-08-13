5fdf13ebe223e86a47ca4639c6d0d163d524e5a3
mcflugen <mcflugen@gmail.com>
2015-08-13 16:56:08 -0600

    Added a docstring for elev_change.

--- SLR.py
+++ SLR.py
@@ -8,15 +8,33 @@ import numpy as np
 
 
 def elev_change(current_SL, n, riv_i, riv_j, ch_depth):
-    # creates river course flag array
-    rc_flag = np.zeros(n.shape, dtype=np.int)
-    rc_flag[riv_i, riv_j] = 1
-    
+    """Raise elevations to sea level if below sea level.
+
+    Set elevations of cells below sea level to sea level unless they are
+    within the river channel.
+
+    Parameters
+    ----------
+    current_SL : float
+        Sea level.
+    n : ndarray
+        Array of elevations.
+    riv_i : ndarray of int
+        Row indices into *n* for river cells.
+    riv_j : ndarray of int
+        Column indices into *n* for river cells.
+    ch_depth : float
+        Channel depth.
+    """
     # changes elevation of last river course cell according to sea level change
     n[riv_i[-1], riv_j[-1]] = current_SL - ch_depth
 
+    channel_elevations = n[riv_i, riv_j].copy()
+
     # raises cell elevation to sea level if it is below
-    n[(n < current_SL) & (rc_flag == 0)] = current_SL
+    n[n < current_SL] = current_SL
+
+    n[riv_i, riv_j] = channel_elevations
 
     # Need to somehow change above treatment of cells... they don't need to be
     # raised to sea level anymore. 
@@ -25,5 +43,3 @@ def elev_change(current_SL, n, riv_i, riv_j, ch_depth):
 #    for I in range(jmax):
 #        n[0][I] = n[0][I] + (IRR)
 #        I = I + 1
-
-    return n
