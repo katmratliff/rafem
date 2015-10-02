9dc44003c35f0b90876160a111b2a824bd354673
katmratiff <k.ratliff@duke.edu>
2015-10-02 10:38:40 -0400

    added ez_setup and setup files

---
+++ setup.py
@@ -0,0 +1,14 @@
+#! /usr/bin/env python
+from ez_setup import use_setuptools
+use_setuptools()
+from setuptools import setup, find_packages
+
+
+setup(name='bmi-avulsion',
+      version='0.1.0',
+      author='Katherine Ratliff',
+      author_email='k.ratliff@duke.edu',
+      description='BMI Avulsion Module',
+      long_description=open('README.md').read(),
+      packages=find_packages(),
+)
