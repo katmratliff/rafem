f3bd4936dc2d1ded4ae780140737af4521e2fb4c
katmratiff <k.ratliff@duke.edu>
2015-12-04 11:14:44 -0500

    changed downcut routine, flux
    still needs to be improved to use find_beach_length (pass prior river
    values).
    
    updated flux to be dependent on two cells prior to river mouth.

--- rafem/downcut.py
+++ rafem/downcut.py
@@ -5,6 +5,7 @@ Created on Tue Mar 17 21:22:00 2015
 @author: kmratliff
 """
 import numpy as np
+import math
 
 from avulsion_utils import get_link_lengths
 from avulsion_utils import find_beach_length
@@ -16,15 +17,22 @@ def cut_init(riv_i, riv_j, n, init_cut):
     return n
 
 
-def cut_new(riv_i, riv_j, n, current_SL, ch_depth, slope, dx=1., dy=1.):
+def cut_new(riv_i, riv_j, n, sea_level, ch_depth, slope, dx=1., dy=1.):
     """Set elevations of new portions of a profile."""
 
     # downcut last river cell by a channel depth
     n[riv_i[-1], riv_j[-1]] -= ch_depth
 
-    beach_len = find_beach_length(n, (riv_i[-2], riv_j[-2]),
-                                  (riv_i[-1], riv_j[-1]), sea_level,
-                                  ch_depth, slope, dx=dx, dy=dy)
+    last_elev = n[riv_i[-1], riv_j[-1]] + ch_depth - sea_level
+    max_cell_h = math.tan(slope) * dx
+
+    beach_len = (last_elev / max_cell_h)
+
+    ### NEED TO UPDATE INPUTS SO WE CAN USE BEACH LENGTH
+    ### (JUST PASS PRIOR RIVER CELL, TOO) ###
+    # beach_len = find_beach_length(n, (riv_i[-2], riv_j[-2]),
+    #                               (riv_i[-1], riv_j[-1]), sea_level,
+    #                               ch_depth, slope, dx=dx, dy=dy)
     
     if riv_i.size > 1:
         lengths = get_link_lengths((riv_i, riv_j), dx=dx, dy=dy)
