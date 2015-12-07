7e106b4cab22bad322a43c8fd68d809ef60dee64
katmratiff <k.ratliff@duke.edu>
2015-12-07 09:32:31 -0500

    changed flux calculations back to last two river cells

--- rafem/flux.py
+++ rafem/flux.py
@@ -10,20 +10,21 @@ from avulsion_utils import find_beach_length
 def calc_qs(nu, riv_i, riv_j, n, sea_level, ch_depth, dx, dy, dt, slope):
     """Calculate sediment flux at river mouth."""
 
-    # beach_len = find_beach_length(n, (riv_i[-2], riv_j[-2]),
-    #                               (riv_i[-1], riv_j[-1]), sea_level,
-    #                               ch_depth, slope, dx=dx, dy=dy)
+    beach_len = find_beach_length(n, (riv_i[-2], riv_j[-2]),
+                                  (riv_i[-1], riv_j[-1]), sea_level,
+                                  ch_depth, slope, dx=dx, dy=dy)
 
-    # ds = get_link_lengths((riv_i[-2:], riv_j[-2:]), dx=dx, dy=dy)
-    # dz = n[riv_i[-1], riv_j[-1]] - n[riv_i[-2], riv_j[-2]]
+    ds = get_link_lengths((riv_i[-2:], riv_j[-2:]), dx=dx, dy=dy)
+    ds[-1] = ds[-1]/2 + beach_len
+    dz = n[riv_i[-1], riv_j[-1]] - n[riv_i[-2], riv_j[-2]]
     
-    # return - nu * dz / (ds/2 + beach_len)
-
-    ds = get_link_lengths((riv_i[-3:-1], riv_j[-3:-1]), dx=dx, dy=dy)
-    
-    dz = n[riv_i[-2], riv_j[-2]] - n[riv_i[-3], riv_j[-3]]
-
     return - nu * dz / ds
+
+    # ds = get_link_lengths((riv_i[-3:-1], riv_j[-3:-1]), dx=dx, dy=dy)
+    
+    # dz = n[riv_i[-2], riv_j[-2]] - n[riv_i[-3], riv_j[-3]]
+
+    # return - nu * dz / ds
 
 
 
