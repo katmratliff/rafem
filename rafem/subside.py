3ce995412ae07565b663ca76290ce1757539a256
katmratiff <k.ratliff@duke.edu>
2016-09-21 10:22:37 -0400

    fix bug in subside where n array is copied not adjusted

--- rafem/subside.py
+++ rafem/subside.py
@@ -10,9 +10,9 @@ def linear_subsidence(n, riv_i, riv_j, ch_depth, sub_rate, sub_start, SL):
     subside_cells = np.zeros_like(n)
     subside_cells[sub_start:,:] = 1
 
-    subaerial_elev = n
+    subaerial_elev = np.copy(n)
     subaerial_elev[riv_i, riv_j] += ch_depth
 
-    subside_cells[n <= SL] = 0
+    subside_cells[subaerial_cells <= SL] = 0
 
     n[subside_cells == 1] -= sub_rate
