1e5becbc22b5146140d35493e238ee07cbcae497
katmratiff <k.ratliff@duke.edu>
2016-09-21 09:53:15 -0400

    get rid of pu.db lines

--- rafem/subside.py
+++ rafem/subside.py
@@ -1,14 +1,11 @@
 #! /usr/local/bin/python
 
 import numpy as np
-import pudb
 
 
 def linear_subsidence(n, riv_i, riv_j, ch_depth, sub_rate, sub_start, SL):
     """ Subside cells in rows beyond start location by linear rate.
     Updated version could be to subside at an increasing rate towards ocean. """
-
-    pu.db
 
     subside_cells = np.zeros_like(n)
     subside_cells[sub_start:,:] = 1
