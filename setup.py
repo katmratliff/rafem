084f70c87cf4a22d797ac282ba7f074802f6e6b3
mcflugen <mcflugen@gmail.com>
2018-07-04 10:16:04 -0600

    Install rafem as a pymt component.

--- setup.py
+++ setup.py
@@ -3,6 +3,16 @@ from ez_setup import use_setuptools
 use_setuptools()
 from setuptools import setup, find_packages
 
+from model_metadata.utils import get_cmdclass, get_entry_points
+
+
+pymt_components = [
+    (
+        "BmiRiverModule=rafem:BmiRiverModule",
+        ".bmi",
+    )
+]
+
 
 setup(name='rafem',
       version='0.1.0',
@@ -13,4 +23,6 @@ setup(name='rafem',
       url='https://github.com/katmratliff/avulsion-bmi',
       license='MIT',
       packages=find_packages(),
+      cmdclass=get_cmdclass(pymt_components),
+      entry_points=get_entry_points(pymt_components),
 )
