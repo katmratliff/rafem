881c449947fbc84fb38e905c873ee4c7c0828938
mcflugen <mcflugen@gmail.com>
2019-04-07 14:12:46 -0400

    Remove model_metadata from setup.py.

--- setup.py
+++ setup.py
@@ -1,26 +1,22 @@
 #! /usr/bin/env python
-from setuptools import setup, find_packages
-
-from model_metadata.utils import get_cmdclass, get_entry_points
+from setuptools import find_packages, setup
 
 
-pymt_components = [
-    (
-        "BmiRiverModule=rafem:BmiRiverModule",
-        ".bmi",
-    )
-]
-
-
-setup(name='rafem',
-      version='0.1.0',
-      author='Katherine Ratliff',
-      author_email='k.ratliff@duke.edu',
-      description='River Avulsion Flooplain Evolution Model',
-      long_description=open('README.rst').read(),
-      url='https://github.com/katmratliff/avulsion-bmi',
-      license='MIT',
-      packages=find_packages(),
-      cmdclass=get_cmdclass(pymt_components),
-      entry_points=get_entry_points(pymt_components),
+setup(
+    name="rafem",
+    version="0.1.0",
+    author="Katherine Ratliff",
+    author_email="k.ratliff@duke.edu",
+    description="River Avulsion Flooplain Evolution Model",
+    long_description=open("README.rst").read(),
+    url="https://github.com/katmratliff/avulsion-bmi",
+    license="MIT",
+    install_requires=[
+        "numpy",
+        "pyyaml",
+        "scipy",
+        "six",
+    ],
+    packages=find_packages(),
+    entry_points={"console_scripts": ["rafem=rafem.main:main"]},
 )
